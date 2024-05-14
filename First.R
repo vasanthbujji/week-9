bd820fb27ab23f07e55995ef6ca01300a3f2d3dd
vasanthbujji <162331641+vasanthbujji@users.noreply.github.com>
2024-05-14 15:25:08 +0100

    Commit

---
+++ First.R
@@ -0,0 +1,47 @@
+state_data <- as.data.frame(state.x77)
+state_data
+View(state_data)
+pairs(state_data)
+cor(state_data)
+
+names(state_data)[names(state_data) == "life_exp"] <- "Life_Exp"
+names(state_data)[names(state_data) == "HS Grad"] <- "HS_Grad"
+state_data
+
+library(e1071)
+windows(20,12)
+pairs(state_data, smooth=FALSE, scale=FALSE, density=TRUE,ellipses=FALSE, method="spearman",
+      pch=21, lm=FALSE,cor=TRUE, jiggle =FALSE, factor=2, hist.col=4,stars=TRUE,ci =TRUE)
+warnings()
+
+windows(20.12)
+par(mfrow=c(4,3))
+scatter.smooth(x=state_data$Population, y=state_data$Murder,
+               main="Correlation of population  ~Murder",
+               xlab="Population(,000",
+               ylab="Murder")
+scatter.smooth(x=state_data$Income, y=state_data$Murder,
+               main="Correlation of Income  ~Murder",
+               xlab="Income",
+               ylab="Murder%")
+scatter.smooth(x=state_data$Illiteracy, y=state_data$Murder,
+               main="Correlation of Illiteracy  ~Murder",
+               xlab="Illiteracy",
+               ylab="Murder%")
+scatter.smooth(x=state_data$Life_Exp, y=state_data$Murder,
+               main="Correlation of Life_Exp  ~Murder",
+               xlab="Life_Exp",
+               ylab="Murder%")
+scatter.smooth(x=state_data$HS_Grad, y=state_data$Murder,
+               main="Correlation of HS_Grad  ~Murder",
+               xlab="HS_Grad",
+               ylab="Murder%")
+scatter.smooth(x=state_data$Frost, y=state_data$Murder,
+               main="Correlation of Frost  ~Murder",
+               xlab="Frost",
+               ylab="Murder%")
+scatter.smooth(x=state_data$Area, y=state_data$Murder,
+               main="Correlation of Area  ~Murder",
+               xlab="Area",
+               ylab="Murder%")
+
